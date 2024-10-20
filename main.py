d845c61336168fc4e58ae059fb04396b4b22894e
Valerian060 <muhammadhasan4269@gmail.com>
2024-10-20 13:44:35 +0530

    2

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from enum import Enum
  
 # Create a FastAPI application
 app = FastAPI()
  
+class Model(str,Enum):
+ name="name"
+ pas="qere"
+
+
 # Define a route at the root web address ("/")
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+@app.get("/getitems/{itemid}")
+def read_root(itemid : Model):
+    if itemid == Model.name:
+        return {"itemid": Model.name}
+    else:
+       return{'itemid':Model.pas}
