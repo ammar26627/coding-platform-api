f61946f464c9c027a9870fda2ef24507133006dd
Valerian060 <muhammadhasan4269@gmail.com>
2024-10-20 13:54:02 +0530

    2

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ def read_root(itemid : Model):
         return {"itemid": Model.name}
     else:
        return{'itemid':Model.pas}
+    
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
